0d8c4b69284a693cdcdb2fcfcdf7b94bbc855f4b
Nicolas Bocassi <nicolasbocassi@gmail.com>
2022-09-05 18:57:29 -0300

    Templates y viws agregados

--- app_familia/views.py
+++ app_familia/views.py
@@ -1,3 +1,15 @@
+from re import template
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import Template, Context, loader
+from app_familia.models import Familia
 
 # Create your views here.
+
+def lista_familia(request):
+    queryset = Familia.objects.all()
+    diccionario = {'app_familia': queryset}
+    plantilla = loader.get_template('templates1.html')
+    documento_html = plantilla.render(diccionario)
+
+    return HttpResponse(documento_html)
